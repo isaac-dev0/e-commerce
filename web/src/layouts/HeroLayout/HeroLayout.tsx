3818f2f11fb7a9d4d8418f7db41c48eee475ba0b
mrIsaacDolphin <isaac_dolphin@outlook.com>
2023-06-02 14:07:19 +0100

    Added an About Page and modified the layout to meet AC.

--- web/src/layouts/HeroLayout/HeroLayout.tsx
+++ web/src/layouts/HeroLayout/HeroLayout.tsx
@@ -1,9 +1,31 @@
+import { Link, routes } from '@redwoodjs/router'
+
 type HeroLayoutProps = {
   children?: React.ReactNode
 }
 
 const HeroLayout = ({ children }: HeroLayoutProps) => {
-  return <>{children}</>
+  return (
+    <>
+      <header>
+        <nav>
+          <h1 className="text-xl">Nav</h1>
+          <ul>
+            <li>
+              <Link to={routes.home()}>Home</Link>
+            </li>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+      <footer>
+        <h1 className="text-xl">Footer</h1>
+      </footer>
+    </>
+  )
 }
 
 export default HeroLayout
